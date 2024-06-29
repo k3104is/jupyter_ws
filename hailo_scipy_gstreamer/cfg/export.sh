210de40686b01c4ca679a2dba0b71f85019f8656
Satoshi Kataoka <k.3104is@gmail.com>
2024-06-29 13:49:51 +0900

    update

---
+++ hailo_scipy_gstreamer/cfg/export.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# ADD_PATH='/usr/local/builder'
+
+# export PATH=~/.local/bin:$PATH
+# export PATH=${ADD_PATH}:$PATH
--- hailo_scipy_gstreamer/restart.sh
+++ hailo_scipy_gstreamer/restart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 NAME='hailo_scipy_gstreamer'
 
